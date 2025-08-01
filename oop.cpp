a8389c5864ecda915e2e841de4825aee75202deb
mahmoud <mf6396312@gmail.com>
2025-08-01 15:36:41 +0300

    rember me

--- oop.cpp
+++ oop.cpp
@@ -1,14 +1,9 @@
 #include<iostream>
 #include<vector>
 using namespace std
-struct MyStruct
-{
-	int x;
-};
+
 int main()
 {
-	vector<int>arr = { 1,2,3,4,5,6 };
-	cout<<arr.at(2);
-	
+	cout << "iiiiiiiii";
 	return 0
 }
